00835462891076fe0cae7b72ac66326f9df1c6b0
abbliseng <54662679+abbliseng@users.noreply.github.com>
2021-08-31 09:01:07 +0200

    Gravity fixed

--- Assets/Scripts/Celestialbody.cs
+++ Assets/Scripts/Celestialbody.cs
@@ -2,29 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// [RequireComponent(typeof(Rigidbody))]
 public class Celestialbody : MonoBehaviour
 {
-
     public GameObject[] celestialbodies;
     private float gravityConstant = (float)6.67 * Mathf.Pow(10, -11);
-    void Start()
-    {
-        // On start always reset the celestialbody array, loop through and remove self(?) Maybe don't remove self but always just neglect it.
-        // If only the array has changed?
-        if (celestialbodies != GameObject.FindGameObjectsWithTag("GravityEffected"))
-        {
-            celestialbodies = GameObject.FindGameObjectsWithTag("GravityEffected");
-            foreach (GameObject body in celestialbodies)
-            {
-                if (body == this.gameObject)
-                {
-                    // Debug.Log("Found self");
-                }
+
+    void Awake() {
+        int i = 0;
+        celestialbodies = new GameObject[GameObject.FindGameObjectsWithTag("GravityEffected").Length - 1];
+        foreach (GameObject body in GameObject.FindGameObjectsWithTag("GravityEffected")) {
+            if (body != this.gameObject) {
+                celestialbodies[i] = body;
+                i++;
             }
         }
-        // foreach (GameObject body in celestialbodies){
-        //     Debug.Log(body);
-        // }
     }
 
     // Update is called once per frame
@@ -32,15 +24,13 @@ public class Celestialbody : MonoBehaviour
     {
         foreach (GameObject body in celestialbodies)
         {
-            if (body != this.gameObject)
-            {
-                Vector3 difference = this.transform.position - body.transform.position;
-                float dist = difference.magnitude;
-                Vector3 gravityDirection = difference.normalized;
-                float gravity = gravityConstant * (this.GetComponent<Rigidbody>().mass * body.GetComponent<Rigidbody>().mass)/(dist*dist);
-                Vector3 gravityVector = (gravityDirection * gravity);
-                body.GetComponent<Rigidbody>().AddForce(gravityVector, ForceMode.Acceleration);
-            }
+            Vector3 difference = this.transform.position - body.transform.position;
+            float dist = difference.magnitude;
+            Vector3 gravityDirection = difference.normalized;
+            float gravity = gravityConstant * (this.GetComponent<Rigidbody>().mass * body.GetComponent<Rigidbody>().mass)/(dist*dist);
+            Vector3 gravityVector = (gravityDirection * gravity);
+            body.GetComponent<Rigidbody>().AddForce(gravityVector, ForceMode.Acceleration);
+            
         }
     }
 }
