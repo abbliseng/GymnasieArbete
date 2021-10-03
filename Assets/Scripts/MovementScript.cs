6d69d3f6fda53d518cf89f485a564b0ce2c936f1
abbliseng <54662679+abbliseng@users.noreply.github.com>
2021-10-03 12:29:13 +0200

    Camera Movement
    Creative Camera Movement

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -12,6 +12,21 @@ public class MovementScript : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
+
+        speed += Input.mouseScrollDelta.y * 10f;
+        if (speed < 12f) speed = 12f;
+
         controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButton("Jump")) {
+            velocity.y = 1f;
+        } else if(Input.GetKey(KeyCode.LeftShift)) {
+            velocity.y = -1f;
+        } else {
+            velocity.y = 0f;
+        }
+
+
+        controller.Move(velocity * speed * Time.deltaTime);
     }
 }
