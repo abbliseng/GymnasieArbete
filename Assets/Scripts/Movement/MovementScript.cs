07162c70b1ba81b227fcc403c30ce6504dda3a74
abbliseng <54662679+abbliseng@users.noreply.github.com>
2021-12-27 19:21:13 +0100

    camera typ funkar

--- Assets/Scripts/Movement/MovementScript.cs
+++ Assets/Scripts/Movement/MovementScript.cs
@@ -6,6 +6,7 @@ public class MovementScript : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 12f;
+
     Vector3 velocity;
 
     private void Update() {
@@ -13,17 +14,39 @@ public class MovementScript : MonoBehaviour
         float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
 
-        speed += Input.mouseScrollDelta.y * 10f;
-        if (speed < 12f) speed = 12f;
+        // speed += Input.mouseScrollDelta.y * 10f;
+        // if (speed < 12f) speed = 12f;
 
         controller.Move(move * speed * Time.deltaTime);
 
+        // TODO: Make the camera move backwards and forwards using the scrill wheel.
+
+
+        /* Controll height movement with space and shift
         if (Input.GetButton("Jump")) {
             velocity.y = 1f;
         } else if(Input.GetKey(KeyCode.LeftShift)) {
             velocity.y = -1f;
         } else {
             velocity.y = 0f;
+        }
+        */
+
+        float ScrollWheelChange = Input.GetAxis("Mouse ScrollWheel");           //This little peece of code is written by JelleWho https://github.com/jellewie
+        if (ScrollWheelChange != 0)
+        {                                            //If the scrollwheel has changed
+            float R = ScrollWheelChange * 15;                                   //The radius from current camera
+            float PosX = Camera.main.transform.eulerAngles.x + 90;              //Get up and down
+            float PosY = -1 * (Camera.main.transform.eulerAngles.y - 90);       //Get left to right
+            PosX = PosX / 180 * Mathf.PI;                                       //Convert from degrees to radians
+            PosY = PosY / 180 * Mathf.PI;                                       //^
+            float X = R * Mathf.Sin(PosX) * Mathf.Cos(PosY);                    //Calculate new coords
+            float Z = R * Mathf.Sin(PosX) * Mathf.Sin(PosY);                    //^
+            float Y = R * Mathf.Cos(PosX);                                      //^
+            float CamX = Camera.main.transform.position.x;                      //Get current camera postition for the offset
+            float CamY = Camera.main.transform.position.y;                      //^
+            float CamZ = Camera.main.transform.position.z;                      //^
+            Camera.main.transform.position = new Vector3(CamX + X, CamY + Y, CamZ + Z);//Move the whole player
         }
 
 
