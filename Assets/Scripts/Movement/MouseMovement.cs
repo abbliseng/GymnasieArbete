5d78273cb6828244aa6e9a2827a95f52b2643f0f
abbliseng <54662679+abbliseng@users.noreply.github.com>
2022-02-01 10:20:12 +0100

    movement 0.6

--- Assets/Scripts/Movement/MouseMovement.cs
+++ Assets/Scripts/Movement/MouseMovement.cs
@@ -1,49 +1,88 @@
 using UnityEngine;
+using System.Collections;
 
 public class MouseMovement : MonoBehaviour
 {
-    public float dragSpeed = 20;
-    private Vector3 dragOrigin;
 
-    private float speedMod = 10.0f;//a speed modifier
-    private Vector3 point;//the coord to the point where the camera looks at
+    protected Transform _XForm_Camera;
+    protected Transform _XForm_Parent;
 
-    void Update()
+    protected Vector3 _LocalRotation;
+    protected float _CameraDistance = 10f;
+
+    public float MouseSensitivity = 4f;
+    public float ScrollSensitvity = 2f;
+    public float OrbitDampening = 10f;
+    public float ScrollDampening = 6f;
+
+    public bool CameraDisabled = false;
+
+    [Header("Movement")]
+    public CharacterController characterController;
+    public float speed = 6.0f;
+
+    // Use this for initialization
+    void Start()
     {
-        // Raycast setup
-        RaycastHit hit;
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        LayerMask mask = LayerMask.GetMask("RaycastHitPlane");
-        
-        // Drag camera movement
-        if (Input.GetMouseButtonDown(1))
-        {
-            dragOrigin = Input.mousePosition;
-            return;
-        }
-
-        if (Input.GetMouseButton(1))
-        {
-            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-            Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
-
-            transform.Translate(move, Space.World);
-        }
-
-        // Rotate around point
-        if (Input.GetMouseButtonDown(2) && Physics.Raycast(ray, out hit, 1000.0f, mask))
-        {
-            if (hit.transform.gameObject.tag == "PlacingPlane")
-            {
-                point = hit.point;//get target's coords
-                transform.LookAt(point);//makes the camera look to it
-            }
-        }
-        if (Input.GetMouseButton(2))
-        {
-            transform.RotateAround(point, new Vector3(0.0f, 1.0f, 0.0f), 20 * Time.deltaTime * speedMod);
-        }
+        this._XForm_Camera = this.transform;
+        this._XForm_Parent = this.transform.parent;
     }
 
+    void LateUpdate()
+    {
+        // Movement
 
+        // https://www.youtube.com/watch?v=4HpC--2iowE&ab_channel=Brackeys
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x,direction.z) * Mathf.Rad2Deg + C
+        }
+
+        // Handles rotation
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            CameraDisabled = !CameraDisabled;
+
+        if (!CameraDisabled)
+        {
+            if(Input.GetMouseButton(1))
+            {
+                //Rotation of the Camera based on Mouse Coordinates
+                if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+                {
+                    _LocalRotation.x += Input.GetAxis("Mouse X") * MouseSensitivity;
+                    _LocalRotation.y += Input.GetAxis("Mouse Y") * MouseSensitivity * -1;
+
+                    //Clamp the y Rotation to horizon and not flipping over at the top
+                    if (_LocalRotation.y < 0f)
+                        _LocalRotation.y = 0f;
+                    else if (_LocalRotation.y > 90f)
+                        _LocalRotation.y = 90f;
+                }
+            }
+            //Zooming Input from our Mouse Scroll Wheel
+            if (Input.GetAxis("Mouse ScrollWheel") != 0f)
+            {
+                float ScrollAmount = Input.GetAxis("Mouse ScrollWheel") * ScrollSensitvity;
+
+                ScrollAmount *= (this._CameraDistance * 0.3f);
+
+                this._CameraDistance += ScrollAmount * -1f;
+
+                this._CameraDistance = Mathf.Clamp(this._CameraDistance, 1.5f, 100f);
+            }
+        }
+
+        //Actual Camera Rig Transformations
+        Quaternion QT = Quaternion.Euler(_LocalRotation.y, _LocalRotation.x, 0);
+        this._XForm_Parent.rotation = Quaternion.Lerp(this._XForm_Parent.rotation, QT, Time.deltaTime * OrbitDampening);
+
+        if (this._XForm_Camera.localPosition.z != this._CameraDistance * -1f)
+        {
+            this._XForm_Camera.localPosition = new Vector3(0f, 0f, Mathf.Lerp(this._XForm_Camera.localPosition.z, this._CameraDistance * -1f, Time.deltaTime * ScrollDampening));
+        }
+    }
 }
