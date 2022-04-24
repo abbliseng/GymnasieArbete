bfae16d087542aba8303801770bb24a3ba08d259
abbliseng <54662679+abbliseng@users.noreply.github.com>
2022-04-24 21:38:14 +0200

    fair enough

--- Assets/Scripts/Celestials/Celestial.cs
+++ Assets/Scripts/Celestials/Celestial.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Celestial : MonoBehaviour
 {
+    public Planet planet;
     private void Start()
     {
         Debug.Log(this.name + ": Starting");
+        if (planet != null)
+        {
+            planet.GeneratePlanet();
+        }
     }
 }
