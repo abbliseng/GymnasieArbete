b63e5a0f138db4aad16d7dcab64c62f006b56f3c
abbliseng <54662679+abbliseng@users.noreply.github.com>
2022-08-04 15:13:49 +0200

    double click and follow planet

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,49 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     CharacterController controller;
+    public Camera cam;
+    public float doubleClickResetTime = 1f;
+    public bool following = false;
+
+    private float doubleClickTime = 0f;
+    private float raycastMaxDistance = 100000.0f;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
         controller.detectCollisions = false;
+    }
+
+    private void Update()
+    {
+        doubleClickTime -= Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Mouse0) && doubleClickTime > 0f)
+        {
+            doubleClickTime = 0f;
+            RaycastSelect();
+        }else if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            doubleClickTime = doubleClickResetTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+        {
+            following = false;
+            this.transform.SetParent(null);
+        }
+    }
+
+    void RaycastSelect()
+    {
+        RaycastHit hit;
+        var ray = cam.ScreenPointToRay(Input.mousePosition);
+        LayerMask mask = LayerMask.GetMask("RaycastPlanetPlane");
+
+        if (Physics.Raycast(ray, out hit, raycastMaxDistance, mask))
+        {
+            Debug.Log(hit.transform.name);
+            this.transform.SetParent(hit.transform);
+            following = true;
+        }
     }
 }
